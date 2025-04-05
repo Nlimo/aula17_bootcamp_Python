b3a7b7fe3d64bee1a3976b4c69ee701ca9476e5f
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-05 15:17:16 -0300

    arrumando codigo

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 
 # Conectar ao SQLite em memória
-engine = create_engine ('sqlite://meubancodb', echo = True)
+engine = create_engine('sqlite:///meubancodb.db', echo=True)
+
 
 print('Coneção de sistema estabelecida')
