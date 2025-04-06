02f7ce28e002b51f6cc415812afffb373b898fbb
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-06 17:48:27 -0300

    add codes of creat tables in the new data base

--- desafio.py
+++ desafio.py
@@ -4,3 +4,31 @@ from sqlalchemy import create_engine
 engine = create_engine('sqlite:///meubancodbdesafio.db', echo=True)
 
 print('Coneção de sistema estabelecida')
+
+# Criando minhas tabelas
+
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
+
+class Produto(Base):
+    __tablename__  = 'produtos'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    valor = Column(Integer)
+
+# Opcional: backref para acessar os fornecedores do produto
+fornecedores = relationship("Fornecedor", back_populates="produto")
+
+
+class Fornecedor(Base):
+    __tablename__  = 'fornecedores'
+
+    id = Column(Integer, primary_key=True)
+    id_produto = Column(Integer, ForeignKey('produtos.id'))
+
+# Relacionamento com Produto
+produto = relationship("Produto", back_populates="fornecedores")
+
