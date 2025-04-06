fe28a7bbe5a24646d3e3bebd1d82cabbbf8d9454
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-06 18:53:57 -0300

    add code of insert datas

--- desafio.py
+++ desafio.py
@@ -29,10 +29,37 @@ class Fornecedor(Base):
 
     id = Column(Integer, primary_key=True)
     nome = Column(String)
-    id_produto = Column(Integer, ForeignKey('produtos.id'))
 
 # Relacionamento com Produto
 produto = relationship("Produto", back_populates="fornecedores")
 
 # Criando banco com as tabelas
 Base.metadata.create_all(engine)
+
+
+#################################################
+# ADD DADOS 
+#################################################
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+
+with Session() as session:
+
+    fornecedor = [
+        Fornecedor(id = 1, nome = 'Depósito 01'),
+        Fornecedor(id = 2, nome = 'Depósito 02')
+    ]
+
+    session.add_all(fornecedor) #add_all deve ser utlizado pois é uma lista de objetos, se fosse so um, seria add()
+    session.commit()
+
+    produto = [
+        Produto(id = 11, nome = 'Coca-Cola', valor = 5),
+        Produto(id = 12, nome = 'Pepsi', valor = 4),
+        Produto(id = 13, nome = 'Pepsi', valor = 4),
+    ]
+
+    session.add_all(produto)
+    session.commit()
